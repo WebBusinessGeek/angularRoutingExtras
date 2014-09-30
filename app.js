a7d9df1d06b4aae46bfa3178239a176b5090bb7b
Kevin Washington <kevw12188@gmail.com>
2014-09-30 17:26:31 -0400

    added ngRoute and files

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-var app = angular.module('app', [])
+var app = angular.module('app', ['ngRoute'])
+
+
+app.controller('routeParameterExample', function($scope, $routeParams){
+
+})
+
+
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+			template: 'this is the homepage'
+		})
+		.when('/not', {
+			template: 'this is not the homepage'
+		});
+
+})
